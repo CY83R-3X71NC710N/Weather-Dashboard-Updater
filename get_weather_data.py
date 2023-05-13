84c94afb4a6e9939ed8c51be18762bfcfa091742
Gavin Klibowitz <103222783+CY83R-3X71NC710N@users.noreply.github.com>
2023-05-13 18:44:31 -0500

    Update get_weather_data.py

--- get_weather_data.py
+++ get_weather_data.py
@@ -15,24 +15,31 @@ response = requests.get(url)
 # Get the weather data
 weather_data = response.json()
 
-# Extract the data you want to save to the CSV file
-data = {
-    "date": weather_data.get("dt", "N/A"),
-    "temperature": weather_data["main"]["temp"],
-    "humidity": weather_data["main"]["humidity"],
-    "wind_speed": weather_data["wind"]["speed"],
-    "min_temp": weather_data["main"]["temp_min"],
-    "max_temp": weather_data["main"]["temp_max"],
-    "pressure": weather_data["main"]["pressure"],
-    "visibility": weather_data.get("visibility", "N/A"),
-    "cloud_coverage": weather_data["clouds"]["all"],
-    "sunrise_time": weather_data["sys"]["sunrise"],
-    "sunset_time": weather_data["sys"]["sunset"],
-    "weather_description": weather_data["weather"][0]["description"],
-    "weather_icon": weather_data["weather"][0]["icon"],
-}
+# Check if the "main" key exists in the weather data
+if "main" in weather_data:
+    main_data = weather_data["main"]
 
-# Save the data to a CSV file
-with open("weather.csv", "a") as csv_file:
-    writer = csv.DictWriter(csv_file, fieldnames=data.keys())
-    writer.writerow(data)
+    # Extract the data you want to save to the CSV file
+    data = {
+        "date": weather_data.get("dt", "N/A"),
+        "temperature": main_data.get("temp", "N/A"),
+        "humidity": main_data.get("humidity", "N/A"),
+        "wind_speed": weather_data["wind"].get("speed", "N/A"),
+        "min_temp": main_data.get("temp_min", "N/A"),
+        "max_temp": main_data.get("temp_max", "N/A"),
+        "pressure": main_data.get("pressure", "N/A"),
+        "visibility": weather_data.get("visibility", "N/A"),
+        "cloud_coverage": weather_data["clouds"].get("all", "N/A"),
+        "sunrise_time": weather_data["sys"].get("sunrise", "N/A"),
+        "sunset_time": weather_data["sys"].get("sunset", "N/A"),
+        "weather_description": weather_data["weather"][0].get("description", "N/A"),
+        "weather_icon": weather_data["weather"][0].get("icon", "N/A"),
+    }
+
+    # Save the data to a CSV file
+    with open("weather.csv", "a") as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=data.keys())
+        writer.writerow(data)
+else:
+    print("Error: Unable to retrieve weather data.")
+
