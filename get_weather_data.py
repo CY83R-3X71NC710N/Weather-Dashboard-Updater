c275bf55b9b54c2d9c72e81df68de6473b68566e
Gavin Klibowitz <103222783+CY83R-3X71NC710N@users.noreply.github.com>
2023-05-13 18:49:24 -0500

    Update get_weather_data.py

--- get_weather_data.py
+++ get_weather_data.py
@@ -16,20 +16,14 @@ response = requests.get(url)
 weather_data = response.json()
 
 # Extract the data you want to save to the CSV file
-data = {
-    "date": weather_data.get("dt", "N/A"),
-    "temperature": weather_data["main"].get("temp", "N/A"),
-    "feels_like": weather_data["main"].get("feels_like", "N/A"),
-    "humidity": weather_data["main"].get("humidity", "N/A"),
-    "wind_speed": weather_data["wind"].get("speed", "N/A"),
-    "wind_deg": weather_data["wind"].get("deg", "N/A"),
-    "visibility": weather_data.get("visibility", "N/A"),
-    "cloud_coverage": weather_data["clouds"].get("all", "N/A"),
-    "sunrise_time": weather_data["sys"].get("sunrise", "N/A"),
-    "sunset_time": weather_data["sys"].get("sunset", "N/A"),
-    "weather_description": weather_data["weather"][0].get("description", "N/A"),
-    "weather_icon": weather_data["weather"][0].get("icon", "N/A"),
-}
+data = {}
+
+for key, value in weather_data.items():
+    if isinstance(value, dict):
+        for sub_key, sub_value in value.items():
+            data[sub_key] = sub_value
+    else:
+        data[key] = value
 
 # Save the data to a CSV file
 with open("weather.csv", "a") as csv_file:
